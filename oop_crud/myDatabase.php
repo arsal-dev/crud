e575e90f7968800ca895af1a64f8e8e9199ab734
unknown <arsalrajput173@gmail.com>
2023-09-12 15:37:39 +0500

    oop crud completed

--- oop_crud/myDatabase.php
+++ oop_crud/myDatabase.php
@@ -13,12 +13,16 @@ class myDatabase
         }
     }
 
-    function create($table, $name, $email, $password)
+    function create($table, $params)
     {
-        $sql = "INSERT INTO $table (name, email, password) VALUES ('$name', '$email', '$password')";
+        $params_keys = implode(", ", array_keys($params));
+
+        $params_values = implode("', '", $params);
+
+        $sql = "INSERT INTO $table ($params_keys) VALUES ('$params_values')";
 
         if ($this->conn->query($sql)) {
-            return true;
+            return  'Data Submitted Successfuly Id: ' . $this->conn->insert_id;
         } else {
             return false;
         }
@@ -26,25 +30,43 @@ class myDatabase
 
     function read($table, $id = false)
     {
-        if ($id) {
-            $sql = "SELECT * FROM $table WHERE id = $id";
-        } else {
-            $sql = "SELECT * FROM $table";
+        $sql = "SELECT * FROM $table";
+        if ($id != false) {
+            $sql .= " WHERE id = $id";
         }
 
+        $result = $this->conn->query($sql);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    function update($table, $params, $id)
+    {
+        $newArr = [];
+        foreach ($params as $key => $value) {
+            $newArr[] = "$key = '$value'";
+        }
+
+        $params_string = implode(', ', $newArr);
+
+        $sql = "UPDATE $table SET $params_string WHERE id = $id";
+
         if ($this->conn->query($sql)) {
-            return true;
+            return  'Data updated Successfuly Affected Row: ' . $this->conn->affected_rows;
         } else {
             return false;
         }
     }
 
-    function update()
+    function delete($table, $id)
     {
-    }
+        $sql = "DELETE FROM $table WHERE id = $id";
 
-    function delete()
-    {
+        if ($this->conn->query($sql)) {
+            return  'Data deleted Successfuly';
+        } else {
+            return false;
+        }
     }
 
     function __destruct()
@@ -56,4 +78,6 @@ class myDatabase
 
 $myObj = new myDatabase('localhost', 'root', '', 'oop_crud');
 
-echo $myObj->create('users', 'name', 'email@email.com', 12345678);
+// echo $myObj->create('admins', ['username' => 'Ali', 'phone' => '031864648', 'address' => 'Haroon Abad']);
+// print_r($myObj->read('users', 2));
+print_r($myObj->update('users', ['name' => 'Ahmed', 'email' => 'ahmed@email.com', 'password' => '87654321'], 2));
